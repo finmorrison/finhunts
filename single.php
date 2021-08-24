c1548c141ec2348ec3092d28c4add2414ccfd21a
finmorrison <fin.morrison@gmail.com>
2021-08-24 00:55:50 -0600

    single container

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -10,40 +11,46 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
- ?>
-			 <div class="entry-content">
-					<?php the_content(); ?>
-					<?php
-					wp_link_pages(
+				<?php
+				while (have_posts()) :
+					the_post();
+				?>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php
+						wp_link_pages(
+							array(
+								'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
+								'after'  => '</div>',
+							)
+						);
+						?>
+					</div>
+				<?php
+					the_post_navigation(
 						array(
-							'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-							'after'  => '</div>',
+							'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'epicpress') . '</span> <span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'epicpress') . '</span> <span class="nav-title">%title</span>',
 						)
 					);
-					?>
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if (comments_open() || get_comments_number()) :
+						comments_template();
+					endif;
+
+				endwhile; // End of the loop.
+				?>
 			</div>
-	<?php
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'epicpress' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'epicpress' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+		</div>
+	</div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
